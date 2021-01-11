e1080ece4cf99f0e97818852a0beee4004b886e9
Wessel Baltus <wesselbaltus@hotmail.nl>
2021-01-11 12:32:39 +0100

    Dbconnection

---
+++ dbconnector.py
@@ -0,0 +1,14 @@
+from mysql.connector import connect, Error
+import os
+
+
+try:
+  with connect(
+    host="localhost",
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASS")
+  ) as connection:
+    print(connection)
+except Error as e:
+  print(e)
+
