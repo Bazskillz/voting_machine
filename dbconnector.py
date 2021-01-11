401c50f0bcbda0bfd71fcab6a67b419bc9352bcb
Wessel Baltus <wesselbaltus@hotmail.nl>
2021-01-11 12:52:56 +0100

    Made query mockup

--- dbconnector.py
+++ dbconnector.py
@@ -1,6 +1,6 @@
 from mysql.connector import connect, Error
 import os
-
+from datetime import datetime
 
 try:
   with connect(host="localhost",user=os.environ.get("DB_USER"),password=os.environ.get("DB_PASS")) as connection:
@@ -8,3 +8,31 @@ try:
 except Error as e:
   print(e)
 
+now = datetime.now()
+
+FirstName = "test" 
+LastName =  "test"
+VoteOn =    "test"
+date = now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+dataAddQuery="""
+UPDATE
+      voted
+SET
+      FirstName = "%s"
+      LastName = "%s"
+      VoteOn = "%s"
+      Date = "%s"
+""" % (
+  FirstName,
+  LastName,
+  VoteOn,
+  date
+)
+
+print(dataAddQuery)
+
+#with connection.cursor() as cursor:
+ # cursor.execute()
+  #connection.commit()
